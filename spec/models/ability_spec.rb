296ea4e5417fcec3f9a9fe09cb9bbe5d69548d59
Asad Ali Bhatti <asadalibhatti@hotmail.com>
2018-01-15 16:01:39 +0100

    define relationships

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Ability, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should belong_to(:hero) }
+  end
 end
--- spec/models/hero_spec.rb
+++ spec/models/hero_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Hero, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should have_many(:abilities) }
+  end
 end
