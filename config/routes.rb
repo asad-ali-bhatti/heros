c7c3b3140ecaf20060a5c03f949023562f1db82a
Asad Ali Bhatti <asadalibhatti@hotmail.com>
2018-01-15 16:43:55 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope :api do
+    resources :heros, module: :v1 ,only: [:index, :show] do
+      resources :abilities, module: :v1,  only: [:index]
+    end
+   resources :abilities, module: :v1,  only: [:index, :show]
+  end
 end
